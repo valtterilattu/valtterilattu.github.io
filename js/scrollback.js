971c46ec85499fc07d9d304582bbedea2ce79563
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2018-08-06 20:02:39 +0300

    modified function

--- js/scrollback.js
+++ js/scrollback.js
@@ -1,13 +1,4 @@
-jQuery(document).ready(function(){       
-   var scroll_start = 0;
-   var home = jQuery('#home');
-   var offset = home.offset();
-    if (home.length){
-   jQuery(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          jQuery(".nav-wrapper").css('background-color', '#f0f0f0');
-       }
-   });
-    }
-});
+	$(document).scroll(function () {
+	  var $nav = $(".navbar-fixed");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
