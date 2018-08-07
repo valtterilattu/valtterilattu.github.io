da1e072c34cae275af479013050006b32159589a
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2018-08-07 19:37:46 +0300

    Added js

--- js/scrollback.js
+++ js/scrollback.js
@@ -4,3 +4,11 @@
 	  var $navbar = $(".navbar-nav");
 	  $navbar.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
+
+	$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
